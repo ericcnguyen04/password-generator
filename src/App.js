d19b3b9c6775f94087fb784843be9c9634bc5da8
ericcnguyen04 <ericc.win@gmail.com>
2023-06-16 22:28:44 -0400

    added generating password function

--- src/App.js
+++ src/App.js
@@ -2,6 +2,29 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  function generatePassword(length) {
+    // Define the characters that can be used in the password
+    const characters =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+=-{}[]|:;<>,.?/";
+  
+    let password = "";
+    for (let i = 0; i < length; i++) {
+      // Generate a random index to select a character from the available characters
+      const randomIndex = Math.floor(Math.random() * characters.length);
+  
+      // Append the selected character to the password
+      password += characters.charAt(randomIndex);
+    }
+  
+    return password;
+  }
+  
+  // Example usage
+  const password = generatePassword(10); // Generate a password with a length of 10 characters
+  console.log(password);
+
+
   return (
     <div>
       <header className="App-header">
@@ -10,7 +33,7 @@ function App() {
             Password Generator
           </h1>
         </div>
-        
+
         <div className='output'>
           joisju
         </div>
