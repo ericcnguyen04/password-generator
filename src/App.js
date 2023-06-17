22af518b72f08f51926be39469aa7522c36376fd
ericcnguyen04 <ericc.win@gmail.com>
2023-06-16 23:22:47 -0400

    another buttons

--- src/App.js
+++ src/App.js
@@ -46,6 +46,14 @@ function App() {
         <div className="button-box">
           <input type='number' className='length' value={passLength} onChange={(e) => setPassLength(e.target.value)}></input>
           <button className="ten" onClick={() => generatePassword(passLength)}>Generate Password</button>
+          <button class="twelve">Copy</button>
+        </div>
+
+        <div className='text'>
+        So, what makes a good password? First and foremost, a strong password is long. The longer the password, the harder it is for attackers to crack. It's recommended to use a minimum of 12 characters, combining uppercase and lowercase letters, numbers, and special characters.
+        <br></br>
+        <br></br>
+        Complexity is another critical aspect of a strong password. Avoid using easily guessable information like your name, birthdate, or common words. Instead, opt for a combination of unrelated words, use misspellings, or substitute letters with symbols.
         </div>
       </header>
     </div>
