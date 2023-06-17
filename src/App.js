dca257e0e8699af625da044d7705a0682faad4ae
ericcnguyen04 <ericc.win@gmail.com>
2023-06-16 22:24:33 -0400

    woah

--- src/App.js
+++ src/App.js
@@ -5,7 +5,17 @@ function App() {
   return (
     <div>
       <header className="App-header">
-        
+        <div className='header'>
+          <h1>
+            Password Generator
+          </h1>
+        </div>
+        <div className='output'>
+          joisju
+        </div>
+        <div class="button-box">
+          <button class="ten">Border change</button>
+        </div>
       </header>
     </div>
   );
